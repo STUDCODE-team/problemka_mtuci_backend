81f06f031e0b930da5cc15c09061588a3a83b727
Ignatiy Glushkov <ignatii.glushkov@octo.uz>
2026-04-06 21:51:25 +0300

    fix

--- services/reports/src/main.py
+++ services/reports/src/main.py
@@ -31,7 +31,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origin_regex=r"https?://[a-zA-Z0-9-]+\.problemka-mtuci\.tech",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
